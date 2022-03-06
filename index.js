6d021e2ac4aba4a074fbfe8de2dedd5299804888
Braden Little <blittle485@gmail.com>
2022-03-06 01:59:17 -0500

    modified launch protocol for testing

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const app = require("./server");
 app.listen(3000, () => {
-    const host = server.address().address;
-    const port = server.address().port;
-    console.log("WordDuel running at http://%s:%s", host, port);
+    console.log("WordDuel running at 3000");
 })
