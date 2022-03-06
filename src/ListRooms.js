5b0932449719fe2cf2cc039c2e9784170df1ca91
Braden Little <blittle485@gmail.com>
2022-03-06 05:32:47 -0500

    integrated backend to list rooms, added loading

--- src/ListRooms.js
+++ src/ListRooms.js
@@ -5,30 +5,40 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import LaunchIcon from '@mui/icons-material/Launch';
+import Loading from "./Loading"
 import './Dashboard.css';
-
+import "./ListRooms.css"
 function ListRooms(props) {
     const [ allRooms, setRooms ] = React.useState([])
     const [ filteredRooms, setFilteredRooms ] = React.useState([])
+    const [ filter, setFilter ] = React.useState(undefined)
+
+    const joinRoom = React.useCallback((code) => {
+        console.log(code)
+    })
+
     React.useEffect(() => {
-        setInterval(() => {
+        const updateRooms = () => {
             fetch(process.env.REACT_APP_BACKEND_URL + "/open-rooms", {
                 method: "GET",
                 mode: "cors"
             }).then(res => res.json())
             .then(result => {
-                console.log(result)
+                setRooms(result)
             }).catch(error => console.log(error))
-        }, 5000)
-    })
+        }
+        setInterval(() => updateRooms(), 5000)
+    }, [])
     
     return (
-            <>
+            <> 
+            {
+                allRooms.length === 0 ? <Loading type={"spin"} color={"#FFFFFF"}/> : 
                 <List>
-                    {DUMMYJSON.map((item) => (
-                        <ListItem key={item.id}> 
-                            <ListItemButton className='List'sx={[ {bgcolor:'gray'},{'&:hover': {backgroundColor: 'gray', }}]} onClick={() => {console.log("Hello")}}>
-                             {item.text} 
+                    {allRooms.map((item, i) => (
+                        <ListItem key={i}> 
+                            <ListItemButton className='List'sx={[ {bgcolor:'gray'},{'&:hover': {backgroundColor: 'gray', }}]} onClick={() => joinRoom(item.code)}>
+                             {item.name} 
                             <ListItemIcon>
                                 <div style={{position:'absolute',right:'5px',top:'5px'}}>
                                     <LaunchIcon />   
@@ -38,9 +48,10 @@ function ListRooms(props) {
                         </ListItem>
                     ))}
                 </List>
-                </>
-                );
             }
+            </>
+        );
+    }
 
 export default ListRooms
           
