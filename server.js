8a8aab125dbc51afeaacced2abe6bf9f18ac1787
Braden Little <blittle485@gmail.com>
2022-03-06 07:03:55 -0500

    cors 2

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({
 
 // Add headers before the routes are defined
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', req.url);
+    res.setHeader('Access-Control-Allow-Origin', "*, localhost:3000");
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
