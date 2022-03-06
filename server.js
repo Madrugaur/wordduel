ea133095afd602324f1e71ed18f0c11be3dc1db7
Braden Little <blittle485@gmail.com>
2022-03-06 07:00:33 -0500

    random shit

--- server.js
+++ server.js
@@ -12,10 +12,11 @@ app.use(bodyParser.urlencoded({
 
 // Add headers before the routes are defined
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', req.originalUrl);
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
+    
     next();
 });
 /**** MODEL SECTION ****/
