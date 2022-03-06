0868604a097e7cdd0da2fde84900a28e79a5c8f8
Braden Little <blittle485@gmail.com>
2022-03-06 05:51:47 -0500

    loader, API integration

--- src/ListRooms.js
+++ src/ListRooms.js
@@ -9,35 +9,14 @@ import Loading from "./Loading"
 import './Dashboard.css';
 import "./ListRooms.css"
 function ListRooms(props) {
-    const [ allRooms, setRooms ] = React.useState([])
-    const [ filteredRooms, setFilteredRooms ] = React.useState([])
-    const [ filter, setFilter ] = React.useState(undefined)
-
-    const joinRoom = React.useCallback((code) => {
-        console.log(code)
-    })
-
-    React.useEffect(() => {
-        const updateRooms = () => {
-            fetch(process.env.REACT_APP_BACKEND_URL + "/open-rooms", {
-                method: "GET",
-                mode: "cors"
-            }).then(res => res.json())
-            .then(result => {
-                setRooms(result)
-            }).catch(error => console.log(error))
-        }
-        setInterval(() => updateRooms(), 5000)
-    }, [])
-    
+   
     return (
             <> 
             {
-                allRooms.length === 0 ? <Loading type={"spin"} color={"#FFFFFF"}/> : 
                 <List>
-                    {allRooms.map((item, i) => (
+                    {props.list.map((item, i) => (
                         <ListItem key={i}> 
-                            <ListItemButton className='List'sx={[ {bgcolor:'gray'},{'&:hover': {backgroundColor: 'gray', }}]} onClick={() => joinRoom(item.code)}>
+                            <ListItemButton className='List'sx={[ {bgcolor:'gray'},{'&:hover': {backgroundColor: 'gray', }}]} onClick={() => props.joinRoom(item.code)}>
                              {item.name} 
                             <ListItemIcon>
                                 <div style={{position:'absolute',right:'5px',top:'5px'}}>
